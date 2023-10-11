08b13a9eb0c696b6074f91cb18a5ec03ab46625f
SokolEugene <zheksem@gmail.com>
2023-10-11 18:01:30 +0300

    home-work 8 done!

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,14 +7,13 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': {
+            const sortedState = [...state];
             if (action.payload === 'up') {
-                return [...state.sort((a, b) => a.name > b.name ? 1 : -1)]
+                return sortedState.sort((a, b) => a.name > b.name ? 1 : -1);
             } else if (action.payload === 'down') {
-                return [...state.sort((a, b) => a.name < b.name ? 1 : -1)]
+                return sortedState.sort((a, b) => a.name < b.name ? 1 : -1);
             }
-            // by name
-            // sort() создаёт новый массив? или нужно в ручную?...
-            return [...state] // need to fix
+            return sortedState;
         }
         case 'check': {
             // filter() создаёт новый массив? или нужно в ручную?...

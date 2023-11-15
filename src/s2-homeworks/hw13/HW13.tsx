38a2df12ba177b85242c3edb06c916731e7455a3
SokolEugene <zheksem@gmail.com>
2023-11-15 13:35:20 +0200

    home-work 13 done!

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -8,11 +8,6 @@ import error400 from './images/400.svg'
 import error500 from './images/500.svg'
 import errorUnknown from './images/error.svg'
 
-/*
-* 1 - дописать функцию send
-* 2 - дизэйблить кнопки пока идёт запрос
-* 3 - сделать стили в соответствии с дизайном
-* */
 
 const HW13 = () => {
     const [code, setCode] = useState('')
@@ -24,7 +19,7 @@ const HW13 = () => {
         const url =
             x === null
                 ? 'https://xxxxxx.ccc' // имитация запроса на не корректный адрес
-                : 'https://incubator-personal-page-back.herokuapp.com/api/3.0/homework/test'
+                : 'https://samurai.it-incubator.io/api/3.0/homework/test'
 
         setCode('')
         setImage('')
@@ -36,13 +31,31 @@ const HW13 = () => {
             .then((res) => {
                 setCode('Код 200!')
                 setImage(success200)
-                // дописать
+                setInfo('')
+                setText(`${res.data.errorText}`)
 
             })
             .catch((e) => {
-                // дописать
-
-            })
+                    if (x === undefined) {
+                        setCode('Ошибка 400!')
+                        setImage(error400)
+                        setText(`${e.message}`)
+                        setInfo(``)
+                    }
+                    if (x === false) {
+                        setCode('Ошибка 500!')
+                        setImage(error500)
+                        setText(`${e.message}`)
+                        setInfo(``)
+                    }
+                    if (x === null) {
+                        setCode('Error!')
+                        setImage(errorUnknown)
+                        setText(`${e.message}`)
+                        setInfo('')
+                    }
+                }
+            )
     }
 
     return (
@@ -55,8 +68,7 @@ const HW13 = () => {
                         id={'hw13-send-true'}
                         onClick={send(true)}
                         xType={'secondary'}
-                        // дописать
-
+                        disabled={!!info}
                     >
                         Send true
                     </SuperButton>
@@ -64,17 +76,15 @@ const HW13 = () => {
                         id={'hw13-send-false'}
                         onClick={send(false)}
                         xType={'secondary'}
-                        // дописать
-
+                        disabled={!!info}
                     >
                         Send false
                     </SuperButton>
                     <SuperButton
                         id={'hw13-send-undefined'}
-                        onClick={send(undefined)}
+                        onClick={send()}
                         xType={'secondary'}
-                        // дописать
-
+                        disabled={!!info}
                     >
                         Send undefined
                     </SuperButton>
@@ -82,8 +92,7 @@ const HW13 = () => {
                         id={'hw13-send-null'}
                         onClick={send(null)} // имитация запроса на не корректный адрес
                         xType={'secondary'}
-                        // дописать
-
+                        disabled={!!info}
                     >
                         Send null
                     </SuperButton>
@@ -110,5 +119,4 @@ const HW13 = () => {
         </div>
     )
 }
-
 export default HW13
